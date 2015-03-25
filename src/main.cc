b08abf552c3b86fb998a3634d024c4214812047e
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-03-25 14:00:39 +0100

    Basic black window that can be closed.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,23 @@
 
+#include <SFML/Graphics.hpp>
+
+
 int main(int argc, char const *argv[]) {
-  // sf::Window window(sf::VideoMode(800, 600), "My window");
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Lord of the Orb",
+                          sf::Style::Titlebar | sf::Style::Close);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear(sf::Color::Black);
+
+    window.display();
+  }
 
   return 0;
 }
