8d2ee3ec678362a99d490c774ecaa261b78444be
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-03-25 22:30:48 +0100

    Borderless window. Close window when pressing Q.

--- src/main.cc
+++ src/main.cc
@@ -5,14 +5,20 @@
 int main(int argc, char const *argv[]) {
   sf::RenderWindow window(sf::VideoMode(1280, 720),
                           "Lord of the Orb",
-                          sf::Style::Close);
+                          sf::Style::None);
 
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
+        break;
       }
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+      window.close();
+      break;
     }
 
     window.clear(sf::Color::Black);
