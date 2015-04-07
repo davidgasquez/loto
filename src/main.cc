8859985cdcba47c5c45f91e29d4251a4966fdf2f
David Gasquez <davidgasquez@gmail.com>
2015-04-07 22:33:43 +0200

    Update TODO

--- src/main.cc
+++ src/main.cc
@@ -38,10 +38,10 @@ int main(int argc, char const *argv[]) {
   auto enemy = new EnemyUnit();
   enemy->Load();
   enemy->set_position(sf::Vector2f(1200.f, 352.f));
-  enemy->set_movement_speed(sf::Vector2f(-100.f, 0.f));
+  enemy->set_movement_speed(sf::Vector2f(-50.f, 0.f));
   instances->AddInstance(enemy);
 
-  window.setFramerateLimit(60);
+  // window.setFramerateLimit(60);
 
   sf::Clock clock;
 
