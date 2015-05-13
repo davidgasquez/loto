e3a864c4be1f61be8d3b8d7c9286dbac37ce7a3a
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-05-13 10:15:58 +0200

    Restrict towers to tiles.

---
+++ src/base/aliases.h
@@ -0,0 +1,7 @@
+
+#ifndef BASE_ALIASES_H_
+#define BASE_ALIASES_H_
+
+using Vec2f = sf::Vector2f;
+
+#endif // BASE_ALIASES_H_
--- src/game/ingame-ui.cc
+++ src/game/ingame-ui.cc
@@ -2,6 +2,7 @@
 #include "game/ingame-ui.h"
 
 #include "engine/game-manager.h"
+#include "base/aliases.h"
 #include "base/debug.h"
 
 
@@ -9,7 +10,7 @@ void InGameUI::Load() {
   tower_button_.setTexture(*GameManager::GetResourcesManager()->UITowerButton());
   tower_selection_.setTexture(*GameManager::GetResourcesManager()->Tower());
 
-  sf::Vector2f pos(GameManager::GetWindowSize());
+  Vec2f pos(GameManager::GetWindowSize());
   pos.x = (pos.x / 2) - 30;
   pos.y = pos.y - 80;
   tower_button_.setPosition(pos);
@@ -30,7 +31,7 @@ void InGameUI::MouseReleased(sf::Event::MouseButtonEvent event) {
     return;
   }
 
-  sf::Vector2f mouse_pos(event.x, event.y);
+  Vec2f mouse_pos(event.x, event.y);
   if (tower_button_.getGlobalBounds().contains(mouse_pos)) {
     active_ = !active_;
   }
@@ -38,6 +39,11 @@ void InGameUI::MouseReleased(sf::Event::MouseButtonEvent event) {
 
 
 void InGameUI::MouseMoved(sf::Event::MouseMoveEvent event) {
-  sf::Vector2f mouse_pos(event.x, event.y);
+  Vec2f mouse_pos(event.x, event.y);
+
+  Vec2f tile_size(GameManager::GetTileSize());
+  mouse_pos.x = static_cast<unsigned>(mouse_pos.x / tile_size.x) * tile_size.x;
+  mouse_pos.y = static_cast<unsigned>(mouse_pos.y / tile_size.y) * tile_size.y;
+
   tower_selection_.setPosition(mouse_pos);
 }
